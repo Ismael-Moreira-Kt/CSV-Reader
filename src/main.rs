f99a3ad5b6d99484b577d9e3d0bedd7f9de63321
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-06-14 02:29:48 +0100

    feat: Implement the read_from_file function that reads the CSV file and prints each record.

--- src/main.rs
+++ src/main.rs
@@ -14,3 +14,16 @@ fn main() {
     let filename = Path::new(&args[1]);
     let _ = read_from_file(filename.to_str().unwrap_or(""));
 }
+
+
+fn read_from_file(path: &str) -> Result<(), Box<dyn Error>> {
+    let mut reader = csv::Reader::from_path(path)?;
+
+    for result in reader.records() {
+        let record = result?;
+
+        println!("{:?}", record);
+    }
+
+    Ok(())
+}
