e64eaae288818ab1d82f639fdc957a9c17de8b13
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-06-14 02:29:03 +0100

    feat: Added basic program structure, including reading arguments and checking for valid input.

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,16 @@
 use csv;
+use std::{env, error::Error, path::Path, process};
+
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    
+    if args.len() < 2 {
+        println!("Usage: {} <filename>", args[0]);
+        process::exit(0);
+    }
+
+    let filename = Path::new(&args[1]);
+    let _ = read_from_file(filename.to_str().unwrap_or(""));
+}
